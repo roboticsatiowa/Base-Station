790a10d87432f644e3db65f82f50e2580cd1f1a9
Ethan Holter <holterethan@gmail.com>
2023-08-11 13:07:10 -0500

    added driving controller

--- run.py
+++ run.py
@@ -1,44 +1,47 @@
-import pickle
-import socket
-import struct
-from time import sleep
-
-import cv2
-import imutils
-
 # echo-server.py
 
+import socket
+import sys
+from time import sleep
 
-HOST = "192.168.1.13" 
-PORT = 55555  # Port to listen on (non-privileged ports are > 1023)
+import pygame
 
-# AF_INET specifies IPv4        SOCK_STREAM specifies TCP Protocol
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    print(f"Starting server on: {HOST} Port: {PORT}...")
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print("video stream started")
+from src import XBcontroller
 
-        data = b''
 
-        payload_size = struct.calcsize("Q")
-        while True:
-            i = 0
-            packet = conn.recv(4 * 1024)
-            if not packet:
-                break
-            data += packet
-            packed_msg_size = data[:payload_size]
-            data = data[payload_size:]
-            msg_size = struct.unpack("Q", packed_msg_size)[0]
-            while len(data) < msg_size:
-                data += conn.recv(4 * 1024)
-            frame_data = data[:msg_size]
-            data = data[msg_size:]
-            frame = pickle.loads(frame_data)
-            cv2.imshow("Receiving...", frame)
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord('q'):
-                break
+def main():
+
+    try:
+        HOST = sys.argv[1]
+    except IndexError:
+        HOST = input("IPv4: ") # 192.168.1.13
+    try: 
+        PORT = int(sys.argv[2])
+    except IndexError:
+        PORT = int(input("PORT: "))
+
+    pygame.init()
+    cont = XBcontroller.XBcontroller()
+
+    # AF_INET specifies IPv4        SOCK_STREAM specifies TCP Protocol
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        print(f"Starting server on: {HOST} Port: {PORT}...")
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+
+        with conn:
+            print(f"Connected to client {addr[0]} on port {addr[1]}")
+
+            while True:
+                events = pygame.event.get()
+                for event in events:  # allows multiple events to be handled at once
+                    # inp = input("Enter msg:")
+                    inp = cont.getInput(event)
+                    if inp:
+                        conn.sendall(inp.encode('UTF-8'))
+                    sleep(0.01)
+
+
+if __name__ == '__main__':
+    main()
