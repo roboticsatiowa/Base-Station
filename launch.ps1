c0baf09ac4511eac326e6971b0078e609d4a6832
Ethan Holter <holterethan@gmail.com>
2023-08-13 12:00:12 -0500

    cchanged ip

--- launch.ps1
+++ launch.ps1
@@ -1,5 +1,6 @@
-python .\run.py 192.168.1.13 55559 &
 
 python .\run_video.py 192.168.1.13 55555 cam1 &
 python .\run_video.py 192.168.1.13 55556 cam2 &
 python .\run_video.py 192.168.1.13 55558 cam3 &
+
+python .\run.py 192.168.1.13 55559
