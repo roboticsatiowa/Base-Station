2df83f830f3d3fb5a0d8bd97e69addfe80a5202a
Ethan Holter <holterethan@gmail.com>
2023-08-14 11:19:48 -0500

    ad

--- launch.ps1
+++ launch.ps1
@@ -3,4 +3,4 @@ python .\run_video.py 192.168.1.13 55555 cam1 &
 python .\run_video.py 192.168.1.13 55556 cam2 &
 python .\run_video.py 192.168.1.13 55558 cam3 &
 
-python .\run.py 192.168.1.13 55559
+python .\run.py 192.168.1.13 55559 &
