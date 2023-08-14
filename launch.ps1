9ffcaad35c81cd0c7a2bf7f05981b74619e32a93
Ethan Holter <holterethan@gmail.com>
2023-08-14 16:48:04 -0500

    added kill to launch.ps1 - kill.ps1 is redundant

--- launch.ps1
+++ launch.ps1
@@ -1,3 +1,5 @@
+# Stop and delete any previous jobs before starting new ones
+Get-Job -Command "*python*" | Remove-Job
 
 python .\run_video.py 192.168.1.13 55555 cam1 &
 python .\run_video.py 192.168.1.13 55556 cam2 &
