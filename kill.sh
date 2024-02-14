9daf2e05d24b532fbf7da632f44dd90ff08d4ff9
Ethan Holter <holterethan@gmail.com>
2024-02-13 18:22:55 -0600

    updated launch and kill scripts

--- kill.sh
+++ kill.sh
@@ -1,1 +1,7 @@
-ssh robotics@uirobotics.local "tmux kill-session -t ros2" || (echo "WARNING: Failed to connect to remote host")
+#!/bin/bash
+
+# Kill the local session
+tmux kill-session -t ros2
+
+# Kill the remote session (TODO if the tmux session, this will falsely say "Failed to connect to remote host")
+ssh robotics@uirobotics.local "tmux kill-session -t ros2"
--- launch.sh
+++ launch.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 echo "Attempting to connect to remote host"
 ssh robotics@uirobotics.local "source ~/Rover/remote_launch.sh" || (echo "WARNING: Failed to connect to remote host")
 
-source install/setup.bash
-ros2 launch base_station base_station.launch.py
+
+if tmux has-session -t ros2
+    then tmux kill-session -t ros2
+fi
+
+tmux new-session -d -s ros2
+tmux send-keys -t ros2 "source ${SCRIPT_DIR}/install/setup.sh && ros2 launch base_station base_station.launch.py" C-m
